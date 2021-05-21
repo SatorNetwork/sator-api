60998ee28e5bd34933fee32a4726bbd61e101f7d
Dmitry Momot <mail@dmomot.com>
2021-05-21 19:35:23 +0300

    WIP: SAT-44

---
+++ svc/challenge/endpoints.go
@@ -0,0 +1,53 @@
+package challenge
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/SatorNetwork/sator-api/internal/jwt"
+	"github.com/go-kit/kit/endpoint"
+	"github.com/google/uuid"
+)
+
+type (
+	// Endpoints collection of profile service
+	Endpoints struct {
+		Example endpoint.Endpoint
+	}
+
+	service interface {
+		Example(ctx context.Context, uid uuid.UUID) (interface{}, error)
+	}
+)
+
+func MakeEndpoints(s service, m ...endpoint.Middleware) Endpoints {
+	e := Endpoints{
+		Example: MakeExampleEndpoint(s),
+	}
+
+	// setup middlewares for each endpoints
+	if len(m) > 0 {
+		for _, mdw := range m {
+			e.Example = mdw(e.Example)
+		}
+	}
+
+	return e
+}
+
+// MakeExampleEndpoint ...
+func MakeExampleEndpoint(s service) endpoint.Endpoint {
+	return func(ctx context.Context, _ interface{}) (interface{}, error) {
+		uid, err := jwt.UserIDFromContext(ctx)
+		if err != nil {
+			return nil, fmt.Errorf("could not get user profile id: %w", err)
+		}
+
+		resp, err := s.Example(ctx, uid)
+		if err != nil {
+			return nil, err
+		}
+
+		return resp, nil
+	}
+}
