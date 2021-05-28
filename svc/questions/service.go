85b313b5118089ae7deac92cceb4b7600b841cf2
ValeriiLav <valerii.lavrushko@gmail.com>
2021-05-28 15:54:07 +0300

    added question options to GetQuestionByChallengeID.

--- svc/questions/service.go
+++ svc/questions/service.go
@@ -37,9 +37,9 @@ type (
 	// QuestionOption struct
 	QuestionOption struct {
 		ID         uuid.UUID `json:"id"`
-		questionID uuid.UUID `json:"question_id"`
-		option     string    `json:"option"`
-		isCorrect  bool      `json:"is_correct"`
+		QuestionID uuid.UUID `json:"question_id"`
+		Option     string    `json:"option"`
+		IsCorrect  bool      `json:"is_correct"`
 	}
 )
 
@@ -54,7 +54,7 @@ func (s *Service) GetQuestionByID(ctx context.Context, id uuid.UUID) (interface{
 		return nil, fmt.Errorf("question with id %w not found", id)
 	}
 
-	answers, err := s.qr.GetAnswersByQuestionID(ctx, id)
+	answers, err := s.qr.GetAnswersByQuestionID(ctx, question.ID)
 	if err != nil {
 		if !db.IsNotFoundError(err) {
 			return nil, fmt.Errorf("could not get answer: %w", err)
@@ -66,20 +66,19 @@ func (s *Service) GetQuestionByID(ctx context.Context, id uuid.UUID) (interface{
 	return castToQuestion(question, answers), nil
 }
 
-func castToQuestion(q repository.Question, answers []repository.QuestionOption) *Question {
-
+func castToQuestion(q repository.Question, a []repository.QuestionOption) *Question {
 	question := &Question{
 		ID:          q.ID,
 		ChallengeID: q.ChallengeID,
 		Question:    q.Question,
 		Order:       q.QuestionOrder,
 	}
-	for i := 0; i < len(answers); i++ {
+	for i := 0; i < len(a); i++ {
 		question.QuestionOptions = append(question.QuestionOptions, QuestionOption{
-			ID:         answers[i].ID,
-			questionID: answers[i].QuestionID,
-			option:     answers[i].QuestionOption,
-			isCorrect:  answers[i].IsCorrect.Bool,
+			ID:         a[i].ID,
+			QuestionID: a[i].QuestionID,
+			Option:     a[i].QuestionOption,
+			IsCorrect:  a[i].IsCorrect.Bool,
 		})
 	}
 
@@ -87,7 +86,8 @@ func castToQuestion(q repository.Question, answers []repository.QuestionOption) 
 }
 
 // GetQuestionByChallengeID returns questions by challenge id
-func (s *Service) GetQuestionByChallengeID(ctx context.Context, id uuid.UUID) (interface{}, error) {
+func (s *Service) GetQuestionByChallengeID(ctx context.Context, id uuid.UUID) (interface{}, error) {  // TODO: Make query that will return required slice.
+	var result []Question
 	questions, err := s.qr.GetQuestionsByChallengeID(ctx, id)
 	if err != nil {
 		if !db.IsNotFoundError(err) {
@@ -97,7 +97,21 @@ func (s *Service) GetQuestionByChallengeID(ctx context.Context, id uuid.UUID) (i
 		return nil, fmt.Errorf("questions with challenge id %w not found", id)
 	}
 
-	return questions, nil
+	for _, q := range questions {
+		answers, err := s.qr.GetAnswersByQuestionID(ctx, q.ID)
+		if err != nil {
+			if !db.IsNotFoundError(err) {
+				return nil, fmt.Errorf("could not get answer: %w", err)
+			}
+
+			return nil, fmt.Errorf("answer with id %w not found", id)
+		}
+
+		question := castToQuestion(q, answers)
+		result = append(result, *question)
+	}
+
+	return result, nil
 }
 
 // CheckAnswer returns question by id
