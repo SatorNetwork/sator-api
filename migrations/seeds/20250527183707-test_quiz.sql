a1aacac1d3f11453c5e0f721cf6c7cef6ba8d67b
Dmitry Momot <dmitrymomot@users.noreply.github.com>
2021-06-01 12:07:51 +0300

    SAT-57: Fixed broken json in websocket transport (#22)
    * WIP: quiz
    
    * WIP: quiz ws
    
    * + quiz messages documentation
    
    * + api doc: added endpoint to get  quiz connnection url
    
    * SAT-57: draft of quiz websocket interactor
    
    * fix servers list
    
    * + test quiz for the hackathon
    
    * + seeds
    
    * + seeds
    
    * + ws new line

---
+++ migrations/seeds/20250527183707-test_quiz.sql
@@ -0,0 +1,77 @@
+-- -------------------------------------------------------------
+-- TablePlus 3.12.8(368)
+--
+-- https://tableplus.com/
+--
+-- Database: pgdb
+-- Generation Time: 2021-06-01 8:46:28.9020 AM
+-- -------------------------------------------------------------
+
+
+INSERT INTO "public"."answer_options" ("id", "question_id", "answer_option", "is_correct", "updated_at", "created_at") VALUES
+('0fb79e84-3a8a-44e5-adc4-4e9e932a5eb2', 'e07427dd-0c8d-4422-95f7-f498da3b6b3a', 'Walmart', 'f', NULL, '2021-06-01 05:42:57.186513'),
+('11aa936f-8e38-464a-9129-33a4192300b8', '9127fcf8-eafa-42c5-9a91-12fcfa36d78a', '11', 'f', NULL, '2021-06-01 05:15:18.418472'),
+('17725672-b654-40ed-8701-3792360b37b8', '9127fcf8-eafa-42c5-9a91-12fcfa36d78a', '9', 'f', NULL, '2021-06-01 05:15:18.418472'),
+('18e6dc8b-6de1-44c9-ba70-ca6ca8e08a93', '15b8dffe-2f20-40ca-9d86-7af111b414f2', 'Phoebe', 'f', NULL, '2021-06-01 05:42:56.883046'),
+('1dea1f9b-60e7-49cf-930b-e890b35509bd', 'a6b247b4-fb05-41f3-bc3b-56132e0f2aea', 'Jack and Phoebe', 'f', NULL, '2021-06-01 05:42:57.062201'),
+('1ec6958d-2ca6-42b3-9c1d-feaeff44ae7d', 'd4d099ca-09ab-438a-a25d-3266d7537065', 'Who knows', 'f', NULL, '2021-06-01 05:42:57.125759'),
+('21c9157b-3fb2-464e-9c67-d314134f46bf', 'f3069ee6-503d-4740-8ecb-716f7a0ed588', 'Rafael', 'f', NULL, '2021-06-01 05:42:57.429776'),
+('290bf27c-7948-4385-90f4-0c3bbf932756', 'f3069ee6-503d-4740-8ecb-716f7a0ed588', 'Chandler', 'f', NULL, '2021-06-01 05:42:57.429776'),
+('330e4369-5fca-4eb2-a8a7-4f3eac0a070e', 'e334fcb5-831f-48ff-87b1-71f3398b6062', 'Minsk', 't', NULL, '2021-06-01 05:42:57.24685'),
+('3406478b-506a-4f7d-b892-859633e049ee', 'a6b247b4-fb05-41f3-bc3b-56132e0f2aea', 'Jack and Judy', 't', NULL, '2021-06-01 05:42:57.062201'),
+('3c2c7cc2-d99f-4c25-b7f1-e98dceebca7c', 'e9a36373-e2c0-44eb-9081-a6ccccc0f305', 'Santa Barbara', 'f', NULL, '2021-06-01 05:42:57.30738'),
+('3de7edec-e64c-450d-915a-42d2cf0cfa8f', 'e334fcb5-831f-48ff-87b1-71f3398b6062', 'Berlin', 'f', NULL, '2021-06-01 05:42:57.24685'),
+('43b36079-0908-433d-845e-bd426761253b', '9127fcf8-eafa-42c5-9a91-12fcfa36d78a', '10', 't', NULL, '2021-06-01 05:15:18.418472'),
+('49d7ce0f-189a-4830-85ef-bfefc84900e6', 'f3069ee6-503d-4740-8ecb-716f7a0ed588', 'Robert', 'f', NULL, '2021-06-01 05:42:57.429776'),
+('4a49d565-8df3-4f14-b843-abf744377bca', '72569f97-26c5-4caf-a9aa-de8b908a8257', 'Orthodontist', 't', NULL, '2021-06-01 05:42:56.940693'),
+('4aaf6b75-06a8-4eb0-b90f-9c895c0168f1', 'e07427dd-0c8d-4422-95f7-f498da3b6b3a', 'Pottery Barn', 't', NULL, '2021-06-01 05:42:57.186513'),
+('4ceebe3f-882d-40b9-94b8-8d8d0ee5779c', '9127fcf8-eafa-42c5-9a91-12fcfa36d78a', '12', 'f', '2021-06-01 05:15:28.642554', '2021-06-01 05:15:18.418472'),
+('4ec9203b-c845-44fc-93a7-2379a06dc9ce', '72569f97-26c5-4caf-a9aa-de8b908a8257', 'Midwife', 'f', NULL, '2021-06-01 05:42:56.940693'),
+('5386ad28-fb0c-4778-be94-f45435ae5c4e', 'd4d099ca-09ab-438a-a25d-3266d7537065', 'Two', 'f', NULL, '2021-06-01 05:42:57.125759'),
+('6b69c628-8a01-49da-a7e8-027dfdd778d7', 'e9a36373-e2c0-44eb-9081-a6ccccc0f305', 'Days of Our Lives', 't', NULL, '2021-06-01 05:42:57.30738'),
+('6d163566-57c1-4047-98c9-e53f25404575', 'e07427dd-0c8d-4422-95f7-f498da3b6b3a', 'Amazon', 'f', NULL, '2021-06-01 05:42:57.186513'),
+('70201228-6654-4d43-9c3a-fc8bd7151f1f', '72569f97-26c5-4caf-a9aa-de8b908a8257', 'Therapist', 'f', NULL, '2021-06-01 05:42:56.940693'),
+('70f903ff-7c68-4a1a-bceb-aaabe4a3e23a', '9127fcf8-eafa-42c5-9a91-12fcfa36d78a', '9', 'f', NULL, '2021-06-01 05:42:56.999275'),
+('73de0955-8680-4e6a-b300-73bd04412995', 'e334fcb5-831f-48ff-87b1-71f3398b6062', 'Moscow', 'f', NULL, '2021-06-01 05:42:57.24685'),
+('7e70cda0-6184-4121-a5f7-d07f36ea9d4c', 'f22f9692-6371-4b53-8f94-66234beb922a', 'Louis Vuitton', 't', NULL, '2021-06-01 05:42:57.36787'),
+('7fd4bd6e-272f-473f-8ff0-9914c6951d44', '9127fcf8-eafa-42c5-9a91-12fcfa36d78a', '10', 't', NULL, '2021-06-01 05:42:56.999275'),
+('859b368d-aa00-4bd4-8b99-32a62527eaf8', '72569f97-26c5-4caf-a9aa-de8b908a8257', 'Dentist', 'f', NULL, '2021-06-01 05:42:56.940693'),
+('85db978b-48f4-4a87-8874-c0a8c9b5af9b', '9127fcf8-eafa-42c5-9a91-12fcfa36d78a', '11', 'f', NULL, '2021-06-01 05:42:56.999275'),
+('87c6282c-5b52-4899-8732-e14395687c01', 'f22f9692-6371-4b53-8f94-66234beb922a', 'Cartier', 'f', NULL, '2021-06-01 05:42:57.36787'),
+('9703fca6-337e-4d01-b536-fbec3f03d5ee', 'e9a36373-e2c0-44eb-9081-a6ccccc0f305', 'Neighbours', 'f', NULL, '2021-06-01 05:42:57.30738'),
+('9c6c6541-84b9-412c-9cab-eb452d3b6cb9', 'e07427dd-0c8d-4422-95f7-f498da3b6b3a', 'Costco', 'f', NULL, '2021-06-01 05:42:57.186513'),
+('aa5e0eda-bcd7-4231-bc9d-78bd1c23e337', 'a6b247b4-fb05-41f3-bc3b-56132e0f2aea', 'John and Jane', 'f', NULL, '2021-06-01 05:42:57.062201'),
+('b2b0303f-1cbe-4a07-860c-ab9b4a3aaaab', '15b8dffe-2f20-40ca-9d86-7af111b414f2', 'Emma', 'f', NULL, '2021-06-01 05:42:56.883046'),
+('b69ed1e3-61b4-4d81-986b-04616542ec4e', 'e334fcb5-831f-48ff-87b1-71f3398b6062', 'Kyiv', 'f', NULL, '2021-06-01 05:42:57.24685'),
+('c714a7d3-4d8e-4789-8092-70084cbd60ec', 'd4d099ca-09ab-438a-a25d-3266d7537065', 'Three', 't', NULL, '2021-06-01 05:42:57.125759'),
+('da416cd0-dd99-49eb-83c1-ce570f86753e', 'f3069ee6-503d-4740-8ecb-716f7a0ed588', 'Richard', 't', NULL, '2021-06-01 05:42:57.429776'),
+('de60203e-dd08-4797-97e3-3a4f3e1d2142', 'e9a36373-e2c0-44eb-9081-a6ccccc0f305', 'General Hospital', 'f', NULL, '2021-06-01 05:42:57.30738'),
+('e407408d-7964-4e6d-ae87-c4a4e8e43a14', '15b8dffe-2f20-40ca-9d86-7af111b414f2', 'Regina', 'f', NULL, '2021-06-01 05:42:56.883046'),
+('e5a6978d-2fc5-42ce-b2c7-e33e046e5d6d', '9127fcf8-eafa-42c5-9a91-12fcfa36d78a', '12', 'f', NULL, '2021-06-01 05:42:56.999275'),
+('e8bc9b21-da8d-4101-8f59-6eef4bea5aa7', '15b8dffe-2f20-40ca-9d86-7af111b414f2', 'Rachel', 't', NULL, '2021-06-01 05:42:56.883046'),
+('ed899cca-3035-4297-a849-82447e19bf09', 'f22f9692-6371-4b53-8f94-66234beb922a', 'Gucci', 'f', NULL, '2021-06-01 05:42:57.36787'),
+('f79de440-f085-445d-9929-7ca764553da5', 'd4d099ca-09ab-438a-a25d-3266d7537065', 'Only one', 'f', NULL, '2021-06-01 05:42:57.125759'),
+('f7bacf93-8f00-4381-a4ed-d9d6e532fa54', 'a6b247b4-fb05-41f3-bc3b-56132e0f2aea', 'Jack and July', 'f', NULL, '2021-06-01 05:42:57.062201'),
+('fa5f1049-6fd6-4515-b66c-baebef75762b', 'f22f9692-6371-4b53-8f94-66234beb922a', 'Zara', 'f', NULL, '2021-06-01 05:42:57.36787');
+
+INSERT INTO "public"."challenges" ("id", "show_id", "title", "description", "prize_pool", "players_to_start", "time_per_question", "updated_at", "created_at") VALUES
+('00cfe310-eb1e-42e5-9d6b-801e288a3f8f', '0899a736-e40d-4ba1-b17e-94857e0c8ff0', 'Miscellaneous ', 'Could you be the biggest Friends fan?', 250, 10, 8, NULL, '2021-06-01 05:06:50.488646'),
+('0a05ab3e-24de-43ba-9c04-3245f635a825', '0899a736-e40d-4ba1-b17e-94857e0c8ff0', 'Miscellaneous #2', 'Could you be the biggest Friends fan?', 300, 10, 8, NULL, '2021-06-01 05:06:50.488646');
+
+INSERT INTO "public"."questions" ("id", "challenge_id", "question", "question_order", "updated_at", "created_at") VALUES
+('15b8dffe-2f20-40ca-9d86-7af111b414f2', '0a05ab3e-24de-43ba-9c04-3245f635a825', 'Who was the maid of honor at Monica’s wedding?', 5, '2021-06-01 05:44:41.509432', '2021-06-01 05:42:56.857224'),
+('72569f97-26c5-4caf-a9aa-de8b908a8257', '0a05ab3e-24de-43ba-9c04-3245f635a825', 'What was the occupation of Rachel’s fiancé Barry Farber?', 4, '2021-06-01 05:44:41.509432', '2021-06-01 05:42:56.912193'),
+('9127fcf8-eafa-42c5-9a91-12fcfa36d78a', '0a05ab3e-24de-43ba-9c04-3245f635a825', 'How many seasons of Friends are there?', 1, NULL, '2021-06-01 05:42:56.969763'),
+('a6b247b4-fb05-41f3-bc3b-56132e0f2aea', '0a05ab3e-24de-43ba-9c04-3245f635a825', 'What are the names of Ross and Monica’s parents?', 3, '2021-06-01 05:44:41.509432', '2021-06-01 05:42:57.02959'),
+('d4d099ca-09ab-438a-a25d-3266d7537065', '0a05ab3e-24de-43ba-9c04-3245f635a825', 'How many times did Ross get divorced?', 2, '2021-06-01 05:44:41.509432', '2021-06-01 05:42:57.092977'),
+('e07427dd-0c8d-4422-95f7-f498da3b6b3a', '00cfe310-eb1e-42e5-9d6b-801e288a3f8f', 'What store does Phoebe hate?', 2, '2021-06-01 05:44:41.509432', '2021-06-01 05:42:57.156172'),
+('e334fcb5-831f-48ff-87b1-71f3398b6062', '00cfe310-eb1e-42e5-9d6b-801e288a3f8f', 'Phoebe’s scientist boyfriend David worked in what city?', 4, '2021-06-01 05:44:41.509432', '2021-06-01 05:42:57.215997'),
+('e9a36373-e2c0-44eb-9081-a6ccccc0f305', '00cfe310-eb1e-42e5-9d6b-801e288a3f8f', 'Joey played Dr. Drake Ramoray on which soap opera show?', 1, '2021-06-01 05:44:41.509432', '2021-06-01 05:42:57.277277'),
+('f22f9692-6371-4b53-8f94-66234beb922a', '00cfe310-eb1e-42e5-9d6b-801e288a3f8f', 'Rachel got a job with which company in Paris?', 3, '2021-06-01 05:44:41.509432', '2021-06-01 05:42:57.338112'),
+('f3069ee6-503d-4740-8ecb-716f7a0ed588', '00cfe310-eb1e-42e5-9d6b-801e288a3f8f', 'Monica dated an ophthalmologist named?', 5, '2021-06-01 05:44:41.509432', '2021-06-01 05:42:57.397667');
+
+INSERT INTO "public"."shows" ("id", "title", "cover", "has_new_episode", "updated_at", "created_at") VALUES
+('0899a736-e40d-4ba1-b17e-94857e0c8ff0', 'Friends', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/friends-tv-show-1542126105.jpg?crop=1.00xw:0.899xh;0,0&resize=1200:*', 'f', '2021-05-26 11:21:02.626882', '2021-05-26 11:19:22.78509'),
+('1729013b-d0e4-4e60-838e-f75ee4973481', 'Peaky Blinders', 'https://public-media.si-cdn.com/filer/7b/ba/7bba298e-7e2e-44f0-adb9-b47dfdc1e240/p05m69vt.jpg', 't', '2021-05-26 11:21:02.626882', '2021-05-25 11:19:22.78509'),
+('17cca2de-cd6e-4bdc-affc-a98461cfdba7', 'Silicon Valley', 'https://miro.medium.com/max/3840/1*F-F_pytxhOLs64a18Fq6cw.jpeg', 'f', '2021-05-26 11:21:02.626882', '2021-05-24 11:19:22.78509');
+
