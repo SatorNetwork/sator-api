60130f1789f6a25a0b38d9d1b724fa8b447c49bc
Valerii Lavrushko <66275034+ValeriiLav@users.noreply.github.com>
2021-06-14 09:20:51 +0300

    SAT-107: Add withdraw rewards transaction + wrap it by client (#33)
    * sat-107: Add withdraw rewards transaction + wrap it by client
    
    * minor refactoring:
    - separate client methods to deposit and withdraw rewards
    - some CS fixes
    
    * fixed rewards migration
    
    Co-authored-by: Dmitry Momot <mail@dmomot.com>

--- svc/rewards/client/client.go
+++ svc/rewards/client/client.go
@@ -3,6 +3,7 @@ package client
 import (
 	"context"
 
+	"github.com/SatorNetwork/sator-api/svc/rewards"
 	"github.com/google/uuid"
 )
 
@@ -13,7 +14,7 @@ type (
 	}
 
 	service interface {
-		AddReward(ctx context.Context, uid uuid.UUID, amount float64, qid uuid.UUID) error
+		AddTransaction(ctx context.Context, uid uuid.UUID, amount float64, qid uuid.UUID, trType int32) error
 		GetUserRewards(ctx context.Context, uid uuid.UUID) (float64, error)
 	}
 )
@@ -23,9 +24,14 @@ func New(s service) *Client {
 	return &Client{s: s}
 }
 
-// AddReward ...
-func (c *Client) AddReward(ctx context.Context, userID uuid.UUID, amount float64, quizID uuid.UUID) error {
-	return c.s.AddReward(ctx, userID, amount, quizID)
+// AddDepositTransaction ...
+func (c *Client) AddDepositTransaction(ctx context.Context, userID uuid.UUID, amount float64, quizID uuid.UUID) error {
+	return c.s.AddTransaction(ctx, userID, amount, quizID, rewards.TransactionTypeDeposit)
+}
+
+// AddWithdrawTransaction ...
+func (c *Client) AddWithdrawTransaction(ctx context.Context, userID uuid.UUID, amount float64) error {
+	return c.s.AddTransaction(ctx, userID, amount, uuid.Nil, rewards.TransactionTypeWithdraw)
 }
 
 // GetUserRewards ...
