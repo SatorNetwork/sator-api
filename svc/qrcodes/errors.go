f3f47ad12532baa5e73bd0dde305830596cfd7c5
Valerii Lavrushko <66275034+ValeriiLav@users.noreply.github.com>
2021-06-14 08:28:40 +0300

    SAT-106: QR-codes service. (#29)
    * SAT-106: QR-codes service.
    
    * fixed comments
    
    * fixed qr-code dates validation + added parameter in qrcodes apidoc
    
    Co-authored-by: Dmitry Momot <mail@dmomot.com>

---
+++ svc/qrcodes/errors.go
@@ -0,0 +1,12 @@
+package qrcodes
+
+import "errors"
+
+var (
+	// ErrInvalidParameter indicates that passed invalid parameter.
+	ErrInvalidParameter = errors.New("invalid parameter")
+
+	// ErrQRCodeExpired indicates that QR code is expired.
+	ErrQRCodeExpired = errors.New("QR code is expired")
+	ErrQRCodeInvalid = errors.New("QR code is not valid yet")
+)
