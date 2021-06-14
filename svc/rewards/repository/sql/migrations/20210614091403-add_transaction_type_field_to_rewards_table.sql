d1521911f46a9f9a5422044ea897a9e6a133ebd1
Dmitry Momot <mail@dmomot.com>
2021-06-14 09:53:56 +0300

    HOTFIX: fixed rewards.transaction_type default value to run migration on the existed database

--- svc/rewards/repository/sql/migrations/20210614091403-add_transaction_type_field_to_rewards_table.sql
+++ svc/rewards/repository/sql/migrations/20210614091403-add_transaction_type_field_to_rewards_table.sql
@@ -1,6 +1,6 @@
 -- +migrate Up
 ALTER TABLE rewards
-ADD COLUMN transaction_type INTEGER NOT NULL;
+ADD COLUMN transaction_type INTEGER NOT NULL DEFAULT 1;
 CREATE INDEX rewards_transaction_type ON rewards USING BTREE (transaction_type);
 -- +migrate Down
 DROP INDEX IF EXISTS rewards_transaction_type;
