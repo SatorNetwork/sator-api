a90538b718d9d405aaf11f9b96ffcfe71050416e
Dmitry Momot <mail@dmomot.com>
2021-06-24 21:24:15 +0300

    hotfix: migrrations error

--- svc/wallet/repository/sql/migrations/20210624180505-add_wallet_type_uniq_index_to_wallets_table.sql
+++ svc/wallet/repository/sql/migrations/20210624180505-add_wallet_type_uniq_index_to_wallets_table.sql
@@ -1,4 +1,4 @@
 -- +migrate Up
-CREATE UNIQUE INDEX wallets_uniq_user_wallet_type ON wallets USING btree(wallet_type);
+CREATE UNIQUE INDEX wallets_uniq_user_wallet_type ON wallets USING btree(user_id, wallet_type);
 -- +migrate Down
 DROP INDEX IF EXISTS wallets_uniq_user_wallet_type;
