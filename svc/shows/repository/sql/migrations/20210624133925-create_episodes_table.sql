73496db3d5b750c911ecfd2cec694cad182c5ea6
ValeriiLav <valerii.lavrushko@gmail.com>
2021-06-29 12:37:57 +0300

    fixed comments

--- svc/shows/repository/sql/migrations/20210624133925-create_episodes_table.sql
+++ svc/shows/repository/sql/migrations/20210624133925-create_episodes_table.sql
@@ -1,6 +1,6 @@
 -- +migrate Up
 CREATE TABLE IF NOT EXISTS episodes (
-    id uuid NOT NULL,
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     show_id uuid NOT NULL,
     episode_number INT NOT NULL DEFAULT 0,
     cover VARCHAR DEFAULT NULL,
@@ -8,8 +8,7 @@ CREATE TABLE IF NOT EXISTS episodes (
     description VARCHAR DEFAULT NULL,
     release_date TIMESTAMP DEFAULT NULL,
     updated_at TIMESTAMP DEFAULT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT now(),
-    PRIMARY KEY(id, show_id)
+    created_at TIMESTAMP NOT NULL DEFAULT now()
 );
 -- +migrate Down
 DROP TABLE IF EXISTS episodes;
