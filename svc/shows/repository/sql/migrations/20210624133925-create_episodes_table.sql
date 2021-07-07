315e3a52c117ef618edb459b9eb36772a898d98f
Dmitry Momot <mail@dmomot.com>
2021-07-07 17:14:37 +0300

    + seasons

--- svc/shows/repository/sql/migrations/20210624133925-create_episodes_table.sql
+++ svc/shows/repository/sql/migrations/20210624133925-create_episodes_table.sql
@@ -1,14 +1,25 @@
 -- +migrate Up
+CREATE TABLE IF NOT EXISTS seasons (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    show_id uuid NOT NULL,
+    season_number INT NOT NULL DEFAULT 0,
+    FOREIGN KEY(show_id) REFERENCES shows(id) ON
+    DELETE CASCADE
+);
 CREATE TABLE IF NOT EXISTS episodes (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     show_id uuid NOT NULL,
+    season_id uuid DEFAULT NULL,
     episode_number INT NOT NULL DEFAULT 0,
     cover VARCHAR DEFAULT NULL,
     title VARCHAR NOT NULL,
     description VARCHAR DEFAULT NULL,
     release_date TIMESTAMP DEFAULT NULL,
     updated_at TIMESTAMP DEFAULT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT now()
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    FOREIGN KEY(season_id) REFERENCES seasons(id) ON
+    DELETE CASCADE
 );
 -- +migrate Down
 DROP TABLE IF EXISTS episodes;
+DROP TABLE IF EXISTS seasons;
