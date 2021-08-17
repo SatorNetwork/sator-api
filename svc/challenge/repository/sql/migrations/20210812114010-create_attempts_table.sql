2ce8f47640ba4dac2d3811075bf7524ab8f4e351
Dmitry Momot <mail@dmomot.com>
2021-08-17 09:10:28 +0300

    hotfix: removed pk for attempts table

--- svc/challenge/repository/sql/migrations/20210812114010-create_attempts_table.sql
+++ svc/challenge/repository/sql/migrations/20210812114010-create_attempts_table.sql
@@ -5,8 +5,7 @@ CREATE TABLE IF NOT EXISTS attempts (
     question_id uuid NOT NULL,
     answer_id uuid DEFAULT NULL,
     valid BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMP DEFAULT NOW(),
-    PRIMARY KEY (user_id, episode_id, question_id)
+    created_at TIMESTAMP DEFAULT NOW()
 );
 -- +migrate Down
 DROP TABLE IF EXISTS attempts;
