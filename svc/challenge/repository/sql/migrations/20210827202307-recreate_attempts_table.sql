60ddddf9f10b2f0c85557897c62f51e2bb48f600
Dmitry Momot <mail@dmomot.com>
2021-08-27 20:31:45 +0300

    fixed duplicate index error when episode access expires

---
+++ svc/challenge/repository/sql/migrations/20210827202307-recreate_attempts_table.sql
@@ -0,0 +1,13 @@
+-- +migrate Up
+DROP TABLE IF EXISTS attempts;
+CREATE TABLE IF NOT EXISTS attempts ( 
+    user_id uuid NOT NULL,
+    episode_id uuid NOT NULL,
+    question_id uuid NOT NULL,
+    answer_id uuid DEFAULT NULL,
+    valid BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+CREATE INDEX attempts_user_episode_question ON episode_access USING BTREE (user_id,episode_id,question_id);
+-- +migrate Down
+DROP TABLE IF EXISTS attempts;
