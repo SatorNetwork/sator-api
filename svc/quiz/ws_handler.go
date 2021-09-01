fc0950077d4b871a99d2559f48f309cae218116e
ValeriiLav <valerii.lavrushko@gmail.com>
2021-09-01 15:36:52 +0300

    added log for sat-326

--- svc/quiz/ws_handler.go
+++ svc/quiz/ws_handler.go
@@ -72,9 +72,10 @@ func QuizWsHandler(s quizService, callback func(uid, qid uuid.UUID), c challenge
 		}
 
 		defer func() {
-			// log.Printf("Defer: %v, QuizID: %v", uid, quizID) // TODO: Remove it!
+			log.Printf("Defer: %v, QuizID: %v", uid, quizID) // TODO: Remove it!
 			quizHub.RemovePlayer(uid)
 			callback(uid, quizID)
+			log.Printf("Callback called //////////////////////") // TODO: Remove it!
 		}()
 
 		fakePlayers := make([]struct {
