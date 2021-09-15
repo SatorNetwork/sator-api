d8ec96221f0145e3ea57207c16c5df3f62255206
ValeriiLav <valerii.lavrushko@gmail.com>
2021-09-15 15:03:48 +0300

    sat-415: Store verification question attempt before user send answer

--- svc/challenge/repository/sql/query/episode_access_attempts.sql
+++ svc/challenge/repository/sql/query/episode_access_attempts.sql
@@ -15,7 +15,7 @@ VALUES ($1, $2, $3, $4, $5) RETURNING *;
 -- name: CountAttempts :one
 SELECT COUNT (*)
 FROM attempts
-WHERE user_id = $1 AND episode_id = $2 AND created_at > $3 AND answer_id IS NOT NULL;
+WHERE user_id = $1 AND episode_id = $2 AND created_at > $3;
 -- name: UpdateAttempt :exec
 UPDATE attempts
 SET answer_id = @answer_id, valid = @valid
