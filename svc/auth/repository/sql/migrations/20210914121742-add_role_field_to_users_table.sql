852a40a2c192c13c8ba53be38a1eed52a197efb5
ValeriiLav <valerii.lavrushko@gmail.com>
2021-09-22 19:00:13 +0300

    fixed comment

--- svc/auth/repository/sql/migrations/20210914121742-add_role_field_to_users_table.sql
+++ svc/auth/repository/sql/migrations/20210914121742-add_role_field_to_users_table.sql
@@ -1,5 +1,5 @@
 -- +migrate Up
 ALTER TABLE users
-    ADD COLUMN role VARCHAR NOT NULL DEFAULT "user";
+    ADD COLUMN role VARCHAR NOT NULL DEFAULT 'user';
 -- +migrate Down
 ALTER TABLE users DROP COLUMN role;
