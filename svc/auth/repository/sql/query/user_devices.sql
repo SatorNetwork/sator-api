e0c73cfe1f5ee44ac479c320c74989e4b424d539
Dmitry Momot <mail@dmomot.com>
2021-11-30 01:14:54 +0200

    +

--- svc/auth/repository/sql/query/user_devices.sql
+++ svc/auth/repository/sql/query/user_devices.sql
@@ -13,10 +13,13 @@ WHERE device_id IN (
 );
 
 -- name: BlockUsersOnTheSameDevice :exec 
-UPDATE users SET disabled = TRUE, block_reason = 'suspicion of fraud: created multiple accounts'
-WHERE device_id IN (
-    SELECT device_id 
-    FROM users_devices 
-    GROUP BY device_id
-    HAVING count(user_id) > 1 
+UPDATE users SET disabled = TRUE, block_reason = 'detected scam: created multiple accounts'
+WHERE id IN (
+    SELECT user_id FROM users_devices
+    WHERE device_id IN (
+        SELECT device_id 
+        FROM users_devices 
+        GROUP BY device_id
+        HAVING count(user_id) > 1 
+    )
 );
