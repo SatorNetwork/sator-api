6bb7a5232439e5485634ceb1233c6bd62f387012
Dmitry Momot <mail@dmomot.com>
2021-12-30 08:23:05 +0200

    added missed primary key

--- svc/shows/repository/sql/migrations/20211223174710-create_reviews_rating_table.sql
+++ svc/shows/repository/sql/migrations/20211223174710-create_reviews_rating_table.sql
@@ -5,8 +5,9 @@ CREATE TABLE IF NOT EXISTS reviews_rating (
     rating_type INT DEFAULT 0,
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     FOREIGN KEY(review_id) REFERENCES ratings(id) ON DELETE CASCADE,
-    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
-    );
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+    PRIMARY KEY(review_id, user_id)
+);
 
 -- +migrate Down
 DROP TABLE IF EXISTS reviews_rating;
