6e36c7f9d6fa199b900d2b0c8b777342d465ead4
Dmitry Momot <mail@dmomot.com>
2022-04-04 14:11:22 +0300

    fixed SAT-730

--- lib/solana/client/errors.go
+++ lib/solana/client/errors.go
@@ -6,5 +6,6 @@ import "errors"
 
 // Predefined package errors
 var (
-	ErrATANotCreated = errors.New("associated token account does not exist")
+	ErrATANotCreated    = errors.New("associated token account does not exist")
+	ErrInvalidPublicKey = errors.New("invalid public key")
 )
