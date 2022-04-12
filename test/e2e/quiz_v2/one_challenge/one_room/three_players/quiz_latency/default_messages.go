6ffaae53770ef45501f7d6d529a7d281cd396ea0
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2022-04-12 15:07:12 +0300

    Added quiz latency test

---
+++ test/e2e/quiz_v2/one_challenge/one_room/three_players/quiz_latency/default_messages.go
@@ -0,0 +1,81 @@
+package quiz_latency
+
+import "github.com/SatorNetwork/sator-api/svc/quiz_v2/message"
+
+var (
+	defaultUserExpectedMessages = []*message.Message{
+		{
+			MessageType:           message.PlayerIsJoinedMessageType,
+			PlayerIsJoinedMessage: &message.PlayerIsJoinedMessage{},
+		},
+		{
+			MessageType:           message.PlayerIsJoinedMessageType,
+			PlayerIsJoinedMessage: &message.PlayerIsJoinedMessage{},
+		},
+		{
+			MessageType:           message.PlayerIsJoinedMessageType,
+			PlayerIsJoinedMessage: &message.PlayerIsJoinedMessage{},
+		},
+		{
+			MessageType:           message.PlayerIsJoinedMessageType,
+			PlayerIsJoinedMessage: &message.PlayerIsJoinedMessage{},
+		},
+		{
+			MessageType: message.CountdownMessageType,
+			CountdownMessage: &message.CountdownMessage{
+				SecondsLeft: 3,
+			},
+		},
+		{
+			MessageType: message.CountdownMessageType,
+			CountdownMessage: &message.CountdownMessage{
+				SecondsLeft: 2,
+			},
+		},
+		{
+			MessageType: message.CountdownMessageType,
+			CountdownMessage: &message.CountdownMessage{
+				SecondsLeft: 1,
+			},
+		},
+		{
+			MessageType: message.CountdownMessageType,
+			CountdownMessage: &message.CountdownMessage{
+				SecondsLeft: 0,
+			},
+		},
+		{
+			MessageType: message.QuestionMessageType,
+			QuestionMessage: &message.QuestionMessage{
+				QuestionText:   "Joey played Dr. Drake Ramoray on which soap opera show?",
+				TimeForAnswer:  1,
+				QuestionNumber: 0,
+				AnswerOptions: []message.AnswerOption{
+					{
+						AnswerText: "Santa Barbara",
+					},
+					{
+						AnswerText: "Neighbours",
+					},
+					{
+						AnswerText: "General Hospital",
+					},
+					{
+						AnswerText: "Days of Our Lives",
+					},
+				},
+			},
+		},
+		{
+			MessageType: message.AnswerReplyMessageType,
+			AnswerReplyMessage: &message.AnswerReplyMessage{
+				Success:    true,
+				SegmentNum: 1,
+			},
+		},
+		{
+			MessageType:         message.WinnersTableMessageType,
+			WinnersTableMessage: &message.WinnersTableMessage{},
+		},
+	}
+)
