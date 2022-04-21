3390d09dc5908bc45146bb4c6fe6b86e8d5e67a4
Ismail14098 <zh.ismayl@gmail.com>
2022-04-21 18:24:59 +0600

    fix all sql

--- svc/quiz/repository/sql/migrations/20210924122148-add_updated_at_to_quiz_answers_table.sql
+++ svc/quiz/repository/sql/migrations/20210924122148-add_updated_at_to_quiz_answers_table.sql
@@ -1,17 +1,17 @@
 -- +migrate Up
 -- +migrate StatementBegin
+ALTER TABLE quiz_answers
+    ADD COLUMN updated_at INTEGER NOT NULL DEFAULT 1;
 CREATE
 OR REPLACE FUNCTION quiz_answers_update_updated_at_column() RETURNS TRIGGER AS $$
 BEGIN NEW .updated_at = NOW();
 RETURN NEW;
 END;
 $$ LANGUAGE 'plpgsql';
--- +migrate StatementEnd
-ALTER TABLE quiz_answers
-ADD COLUMN updated_at INTEGER NOT NULL DEFAULT 1;
 CREATE TRIGGER update_quiz_answers_modtime BEFORE
-UPDATE ON quiz_answers FOR EACH ROW EXECUTE PROCEDURE quiz_answers_update_updated_at_column();
+    UPDATE ON quiz_answers FOR EACH ROW EXECUTE PROCEDURE quiz_answers_update_updated_at_column();
+-- +migrate StatementEnd
 
 -- +migrate Down
-DROP TRIGGER IF EXISTS update_quiz_answers_modtime ON quiz_answers;
-ALTER TABLE quiz_answers DROP COLUMN updated_at;
+-- DROP TRIGGER IF EXISTS update_quiz_answers_modtime ON quiz_answers;
+-- ALTER TABLE quiz_answers DROP COLUMN updated_at;
