f8aea8608d8d547c93dcf8ee97b38899afbea6fa
Dmitry Momot <mail@dmomot.com>
2022-05-27 10:25:54 +0300

    +

--- svc/gapi/transport.go
+++ svc/gapi/transport.go
@@ -108,7 +108,7 @@ func MakeHTTPHandler(gameEndpoints Endpoints, nftPackEndpoints NFTPacksEndpoints
 	r.Get("/settings/{key}", httptransport.NewServer(
 		gameEndpoints.GetSettingsByKey,
 		decodeGetSettingsByKeyRequest,
-		encodeResponse,
+		httpencoder.EncodeResponse,
 		options...,
 	).ServeHTTP)
 
