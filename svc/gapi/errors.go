5344fd863276ef201d110481b12b2865df6c5bd4
Dmitry Momot <mail@dmomot.com>
2022-05-27 22:19:59 +0300

    changed hmac signing method

--- svc/gapi/errors.go
+++ svc/gapi/errors.go
@@ -6,6 +6,7 @@ import "errors"
 var (
 	ErrCouldNotSignResponse          = errors.New("could not sign response")
 	ErrCouldNotVerifySignature       = errors.New("could not verify signature")
+	ErrSignatureInvalid              = errors.New("signature is invalid")
 	ErrNotAllNftsToCraftWereFound    = errors.New("not all nfts to craft were found")
 	ErrNotEnoughNFTsToCraft          = errors.New("not enough nfts to craft")
 	ErrNFTsToCraftHaveDifferentTypes = errors.New("nfts to craft have different types")
--- svc/gapi/signer.go
+++ svc/gapi/signer.go
@@ -1,12 +1,13 @@
 package gapi
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
-	"errors"
 	"fmt"
 
 	"github.com/SatorNetwork/sator-api/lib/httpencoder"
-	"github.com/golang-jwt/jwt"
 )
 
 // SignStruct function is used to sign a struct
@@ -15,15 +16,10 @@ import (
 func SignStruct(signingKey []byte, s interface{}) (string, error) {
 	b, err := json.Marshal(s)
 	if err != nil {
-		return "", fmt.Errorf("could not marshal struct: %w", err)
-	}
-
-	sig, err := jwt.SigningMethodHS256.Sign(string(b), signingKey)
-	if err != nil {
 		return "", ErrCouldNotSignResponse
 	}
 
-	return sig, nil
+	return signHS256(b, signingKey), nil
 }
 
 // ValidateSignature function is used to validate a signature
@@ -35,12 +31,13 @@ func ValidateSignature(signingKey []byte, s interface{}, sig string) error {
 		return fmt.Errorf("could not marshal struct: %v", err)
 	}
 
-	if err := jwt.SigningMethodHS256.Verify(string(b), sig, signingKey); err != nil {
-		if errors.Is(err, jwt.ErrSignatureInvalid) {
-			return err
-		}
-
+	verified, err := verifyHS256(b, signingKey, sig)
+	if err != nil {
 		return fmt.Errorf("%w: %s", ErrCouldNotVerifySignature, err.Error())
+	}
+
+	if !verified {
+		return ErrSignatureInvalid
 	}
 
 	return nil
@@ -50,12 +47,13 @@ func ValidateSignature(signingKey []byte, s interface{}, sig string) error {
 // using the given signing key.
 // Returns an error if the signature is invalid.
 func ValidateStringSignature(signingKey []byte, s string, sig string) error {
-	if err := jwt.SigningMethodHS256.Verify(s, sig, signingKey); err != nil {
-		if errors.Is(err, jwt.ErrSignatureInvalid) {
-			return err
-		}
-
+	verified, err := verifyHS256([]byte(s), signingKey, sig)
+	if err != nil {
 		return fmt.Errorf("%w: %s", ErrCouldNotVerifySignature, err.Error())
+	}
+
+	if !verified {
+		return ErrSignatureInvalid
 	}
 
 	return nil
@@ -81,4 +79,25 @@ func SignResponse(signingKey []byte, response interface{}) (string, error) {
 	}
 
 	return SignStruct(signingKey, structToSign)
+}
+
+// signHS256 signs a message using the HS256 algorithm.
+func signHS256(msg, key []byte) string {
+	mac := hmac.New(sha256.New, key)
+	mac.Write(msg)
+
+	return hex.EncodeToString(mac.Sum(nil))
+}
+
+// verifyHS256 verifies a message using the HS256 algorithm.
+func verifyHS256(msg, key []byte, hash string) (bool, error) {
+	sig, err := hex.DecodeString(hash)
+	if err != nil {
+		return false, err
+	}
+
+	mac := hmac.New(sha256.New, key)
+	mac.Write(msg)
+
+	return hmac.Equal(sig, mac.Sum(nil)), nil
 }
