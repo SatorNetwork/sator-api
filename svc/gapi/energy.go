9ebef942fbdb6068b2dfac9914112c2ffcaf7434
Dmitry Momot <mail@dmomot.com>
2022-05-31 16:07:22 +0300

    fixed default values

--- svc/gapi/energy.go
+++ svc/gapi/energy.go
@@ -1,20 +1,26 @@
 package gapi
 
 import (
-	"log"
 	"time"
 
 	"github.com/SatorNetwork/sator-api/svc/gapi/repository"
 )
 
 func recoveryEnergyPoints(player repository.UnityGamePlayer, energyFull int32, energyRecoveryPeriod time.Duration) int32 {
+	if energyFull == 0 {
+		energyFull = 3
+	}
+
+	if energyRecoveryPeriod == 0 {
+		energyRecoveryPeriod = time.Hour * 4
+	}
+
 	timeSince := time.Since(player.EnergyRefilledAt)
-	log.Printf("timeSince: %v", timeSince.Hours())
 	if player.EnergyPoints >= energyFull || timeSince < energyRecoveryPeriod {
 		return 0
 	}
 
-	recoveryPoints := int32(timeSince.Hours()) / int32(energyRecoveryPeriod.Hours())
+	recoveryPoints := int32(timeSince.Seconds()) / int32(energyRecoveryPeriod.Seconds())
 	if recoveryPoints > 0 {
 		if player.EnergyPoints+int32(recoveryPoints) > energyFull {
 			return energyFull - player.EnergyPoints
--- svc/gapi/energy_test.go
+++ svc/gapi/energy_test.go
@@ -54,6 +54,49 @@ func Test_recoveryEnergyPoints(t *testing.T) {
 			},
 			want: 10,
 		},
+		{
+			name: "recovery points: recovery period is not reached",
+			args: args{
+				player: repository.UnityGamePlayer{
+					EnergyPoints:     0,
+					EnergyRefilledAt: time.Now().Add(-time.Minute * 11),
+				},
+				energyFull:           10,
+				energyRecoveryPeriod: time.Hour,
+			},
+			want: 0,
+		},
+
+		{
+			name: "recovery points: empty energy full",
+			args: args{
+				player: repository.UnityGamePlayer{
+					EnergyPoints:     0,
+					EnergyRefilledAt: time.Now().Add(-time.Hour * 11),
+				},
+				energyRecoveryPeriod: time.Hour,
+			},
+			want: 3,
+		},
+		{
+			name: "recovery points: empty energy recovery period",
+			args: args{
+				player: repository.UnityGamePlayer{
+					EnergyPoints:     0,
+					EnergyRefilledAt: time.Now().Add(-time.Hour * 11),
+				},
+			},
+			want: 2,
+		},
+		{
+			name: "recovery points: empty energy refilled at",
+			args: args{
+				player: repository.UnityGamePlayer{
+					EnergyPoints: 0,
+				},
+			},
+			want: 0,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
