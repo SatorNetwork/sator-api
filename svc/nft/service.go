339a50bb6b3f3736f51622d9c7ece27c54e1075c
Dmitry Momot <mail@dmomot.com>
2022-08-16 00:09:08 +0300

    debug

--- svc/nft/service.go
+++ svc/nft/service.go
@@ -629,7 +629,9 @@ func (s *Service) DoesRelationIDHasNFT(ctx context.Context, relationID uuid.UUID
 // GetNFTsByWalletAddress returns all NFTs that are related to a wallet address
 func (s *Service) GetNFTsByWalletAddress(ctx context.Context, req *GetNFTsByWalletAddressRequest) ([]*NFTListItem, error) {
 	log.Println("GetNFTsByWalletAddress")
-	defer log.Println("GetNFTsByWalletAddress done")
+	defer func() {
+		log.Println("GetNFTsByWalletAddress done")
+	}()
 
 	if !s.enableResourceIntensiveQueries {
 		log.Println("Resource intensive queries are disabled")
